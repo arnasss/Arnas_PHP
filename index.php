753fa8e406c9092f4d087280793c97c68db93e83
Arnas <arnas@users.noreply.github.com>
2019-09-09 12:23:01 +0300

    komentaras

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html>
-<head>
-   <title>Aš varldau HTML!</title>
-</head>
-<body>
-<h1>Arnas - HTML asas</h1>
-<p>Moku kurti statinį turinį.</p>
-</body>
+  <html>
+  <head>
+    <title>Aš varldau HTML!<?php print "ir PHP" ?></title>
+  </head>
+    <body>
+      <h1>Arnas - HTML <?php print "ir PHP" ?> asas</h1>
+        <p>Moku kurti statinį turinį.<br>Atidaryta: <?php print date("Y-m-d h:i:sa") ?></p>
+    </body>
 </html>
