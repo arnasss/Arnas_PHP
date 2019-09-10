8139bd9dbea35819f020073dc57ec99662f00f69
arnasss <arnasile@gmail.com>
2019-09-10 16:38:04 +0300

    Čia yra komentarai

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-<?php
-/**
- * Si eilute pasisveikina
- */
-print 'Labas';
-//print 'Iki';
+<?php 
+
+date_default_timezone_set("Europe/Vilnius");
+
 ?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP lydės ir <?php print date("Y/m/d", strtotime('+1 day'));?></title>
+</head>
+<body>
+    <h1>Arnas - PHP su manim buvo <br> ir <?php print date("H:i:s", strtotime('-1 hour'))?></h1>
+    <p><?php print date('Y', strtotime('+1 year'));?> ne už kalnų!</p>
+</body>
+</html>
 
 <html>
 <head>
@@ -13,6 +21,6 @@ print 'Labas';
 </head>
 <body>
     <h1>Arnas - HTML <?php print 'ir PHP';?> asas <br> jau nuo ,<?php print  date("Y");?> metų</h1>
-    <p>Viskas prasidėjo <?php print  date("m") .'<br>' .'mėnesio,' . date("d") .'dieną!';?> </p>
+    <p>Viskas prasidėjo <?php print  date("m") .'<br>' .'mėnesio, ' . date("d") .' dieną!';?> </p>
 </body>
 </html>
