adc833695031bf94a726a85315b829b7c39b2e57
arnasss <arnasile@gmail.com>
2019-09-11 11:12:03 +0300

    Čia yra komentarai

--- index.php
+++ index.php
@@ -1,26 +1,10 @@
-<?php 
-
-date_default_timezone_set("Europe/Vilnius");
-
-?>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>PHP lydės ir <?php print date("Y/m/d", strtotime('+1 day'));?></title>
+    <title>PHP lydės ir <?php print date('Y/m/d', strtotime('+' . rand(1, 360) . 'haurs')); ?>!</title>
 </head>
 <body>
-    <h1>Arnas - PHP su manim buvo <br> ir <?php print date("H:i:s", strtotime('-1 hour'))?></h1>
-    <p><?php print date('Y', strtotime('+1 year'));?> ne už kalnų!</p>
-</body>
-</html>
-
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title><?php print 'Aš , '. date("l") .' ir PHP';?> </title>
-</head>
-<body>
-    <h1>Arnas - HTML <?php print 'ir PHP';?> asas <br> jau nuo ,<?php print  date("Y");?> metų</h1>
-    <p>Viskas prasidėjo <?php print  date("m") .'<br>' .'mėnesio, ' . date("d") .' dieną!';?> </p>
+    <h1>Arnas - Galbūt turėsiu <?php print rand(1, 4); ?> <br> vaikų(us)!</h1>
+    <p>D. Trump'as nebebus prezidentu: <br> <?php print date('Y/m/d', strtotime('+' . rand(2, 10) . 'year')); ?></p>
 </body>
 </html>
