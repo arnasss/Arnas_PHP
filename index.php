f224936d8c33c5fc29bd3ee9960de484b9c5b406
arnasss <arnasile@gmail.com>
2019-09-12 10:47:47 +0300

    Čia yra komentarai

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $rand1 = rand(1, 100);
 $rand2 = rand(101, 200);
 $rand3 = rand(201, 300);
-$srand4 = rand(301, 400);
+$rand4 = rand(301, 400);
 
 ?>
 <html>
@@ -11,9 +11,9 @@ $srand4 = rand(301, 400);
         <meta charset="UTF-8">
     </head>
     <body>
-        <div class="body1">
+        <div class="body">
             <h1>Skolos skaiciuokle</h1>
-            <h3>Jei paėmei <?php print $rand1; ?> jievru</h3>
+            <h3>Jei paėmei <?php print $rand4; ?> eurų</h3>
             <h3>Su dviem kabančiais gražinsi <?php print $rand3; ?></h3>
             <h3>Su vienu kabančiu gražinsi <?php print $rand2; ?></h3>
         </div>
