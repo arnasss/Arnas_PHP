49aa90bd08aad25ec44557e777d52b1528a2f9fb
arnasss <arnasile@gmail.com>
2019-09-12 10:43:11 +0300

    Čia yra komentarai

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
 
-$img = 'http://qnimate.com/wp-content/uploads/2014/03/images2.jpg';
+$rand1 = rand(1, 100);
+$rand2 = rand(101, 200);
+$rand3 = rand(201, 300);
+$srand4 = rand(301, 400);
 
 ?>
-
 <html>
     <head>
         <meta charset="UTF-8">
     </head>
     <body>
-        <img src="<?php print $img; ?>">
-        <img src="<?php print $img; ?>">
-        <img src="<?php print $img; ?>">
-        <img src="<?php print $img; ?>">
+        <div class="body1">
+            <h1>Skolos skaiciuokle</h1>
+            <h3>Jei paėmei <?php print $rand1; ?> jievru</h3>
+            <h3>Su dviem kabančiais gražinsi <?php print $rand3; ?></h3>
+            <h3>Su vienu kabančiu gražinsi <?php print $rand2; ?></h3>
+        </div>
     </body>
 </html>
