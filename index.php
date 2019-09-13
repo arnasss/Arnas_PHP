fa7fcdc51ac4f9597f6c055fffb7663f464b1d2e
arnasss <arnasile@gmail.com>
2019-09-13 12:53:23 +0300

    sunnyV2

--- index.php
+++ index.php
@@ -16,17 +16,24 @@ if ($sunny) {
                 .sauleta {
                     background-image: url(https://solarsystem.nasa.gov/system/basic_html_elements/x11561_Sun.png.pagespeed.ic.0igPUXQBpc.png);
                     height: 100px;
-                    width: 100px;
+                    width: 130px;
                     background-size: cover;
+                    display: inline-block;
                 }
                 .debesuota {
                     background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_QOa5pS-meIekvdMFUsW_Rwio2Sj9fj8IOR5ylllrJajpSo-j);
                     height: 100px;
-                    width: 100px;
+                    width: 130px;
                     background-size: cover;
+                    display: inline-block;
+                }
+                .ligevimui {
+                    display: inline-block;
+                }
             </style>
         </head>
         <body>
             <div class="<?php print $isvada?>"></div>
+            <h1 class="ligevimui"><?php print $isvada; ?></h1>
     </body>
 </html>
