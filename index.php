da4568852014a310a08c70271c9599656299dd37
arnasss <arnasile@gmail.com>
2019-09-13 16:33:54 +0300

    normalize css pridetas

--- index.php
+++ index.php
@@ -1,39 +1,49 @@
 <?php
 
-$sunny = rand(0, 1);
+$kokia_kava = rand(0, 2);
 
-if ($sunny) {
-    $isvada = 'sauleta';
+if ($kokia_kava === 0) {
+    $kava = 'black-coffe';
+} elseif ($kokia_kava === 1) {
+    $kava = 'latte';
 } else {
-    $isvada = 'debesuota';
+    $kava = 'tee';
 }
+
+$text = "Gersiu $kava";
 
 ?>
 <html>
-        <head>
-            <meta charset="UTF-8">
-            <style>
-                .sauleta {
-                    background-image: url(https://solarsystem.nasa.gov/system/basic_html_elements/x11561_Sun.png.pagespeed.ic.0igPUXQBpc.png);
-                    height: 100px;
-                    width: 130px;
-                    background-size: cover;
-                    display: inline-block;
-                }
-                .debesuota {
-                    background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_QOa5pS-meIekvdMFUsW_Rwio2Sj9fj8IOR5ylllrJajpSo-j);
-                    height: 100px;
-                    width: 130px;
-                    background-size: cover;
-                    display: inline-block;
-                }
-                .ligevimui {
-                    display: inline-block;
-                }
-            </style>
-        </head>
-        <body>
-            <div class="<?php print $isvada?>"></div>
-            <h1 class="ligevimui"><?php print $isvada; ?></h1>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="includes/normalize.css">
+        <style>
+            .black-coffe {
+                background-color: black;
+                color: white;
+                display: flex;
+            }
+            .latte {
+                background-color: red;
+                display: flex;
+            }
+            .tee {
+                background-color: green;
+                display: flex;
+
+            }
+            .ligev {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 100%;
+
+            }
+        </style>
+    </head>
+    <body class="<?php print $kava; ?>">
+        <div class="ligev">
+            <h1><?php print $text; ?></h1>
+        </div>
     </body>
 </html>
