d34d3232f87ede04643e1150c67d6e7a160291b4
arnasss <arnasile@gmail.com>
2019-09-13 12:12:30 +0300

    Ar miegosi ant sofos2

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-
 $grizai_velai = rand(0, 1);
 $grizai_isgeres = rand(0, 1);
-
-$pavadinimas = 'Buitinė skaičiuoklė';
-
 
 if ($grizai_isgeres && $grizai_velai) {
     $ispausdink = 'Grižai vėlai ir išgėres';
@@ -15,14 +11,29 @@ if ($grizai_isgeres && $grizai_velai) {
 } else {
     $ispausdink = 'Nieko nepadarei';
 }
-        
+
+if ($grizai_isgeres && $grizai_velai) {
+    $ispausdink_2 = 'Miegosi';
+} elseif ($grizai_isgeres && !$grizai_velai) {
+    $ispausdink_2 = 'Nemiegosi';
+} elseif (!$grizai_isgeres && $grizai_velai) {
+    $ispausdink_2 = 'Nemiegosi';
+} else {
+    $ispausdink_2 = 'Nemiegosi';
+}
+
+$pavadinimas = 'Buitinė skaičiuoklė';
+$isvada = "Išvada: $ispausdink_2 ant sofos";
+
 ?>
+
 <html>
-    <head>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-        <h1><?php print $pavadinimas; ?></h1>
-        <h2><?php print $ispausdink; ?></h2>
+        <head>
+            <meta charset="UTF-8">
+        </head>
+        <body>
+            <h1><?php print $pavadinimas; ?></h1>
+            <h2><?php print $ispausdink; ?></h2>
+            <h3><?php print $isvada; ?></h3>
     </body>
 </html>
