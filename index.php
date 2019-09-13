eea4a381bdea8424bf994fe87999b7d742b481d5
arnasss <arnasile@gmail.com>
2019-09-13 11:00:59 +0300

    Keliones skaicevimasV2

--- index.php
+++ index.php
@@ -5,16 +5,22 @@ $consumption = 7.5;
 $price_l = 1.3;
 $me_money = 100;
 
-$sunaudota_kelioniai = round(($distance * $consumption) / 100, 2);
-$keliones_kaina = round($sunaudota_kelioniai * $price_l, 2);
+$sunaudota_kelioniai = round(($distance * $consumption)/100, 2);
+$keliones_kaina = round($sunaudota_kelioniai * $price_l , 2);
 if ($keliones_kaina < $me_money) {
-    $text_3 = 'įperkama';
+    $text_7 = 'įperkama';
 } else {
-    $text_3 = 'neįperkama';
-};
+    $text_7 = 'neįperkama';
+}
 
-$text_1 = "Turimi pinigai $me_money";
-$text_2 = "Išvada: Kelionė $text_3";
+
+
+$text_1 = "elionės skaičiuoklė";
+$text_2 = "Nuvažiuota distancija: $distance .";
+$text_3 = "Sunaudota $sunaudota_kelioniai l. kuro";
+$text_4 = "Kaina $keliones_kaina pinigų.";
+$text_5 = "Turimi pinigai $me_money";
+$text_6 = "švada: Kelionė: $text_7";
 
 ?>
 <html>
@@ -22,10 +28,14 @@ $text_2 = "Išvada: Kelionė $text_3";
         <meta charset="UTF-8">
     </head>
     <body>
+        <h1><?php print $text_1; ?></h1>
         <ul>
-            <li><?php print $text_1; ?></li>
+            <li><?php print $text_5; ?></li>
+            <li><?php print $text_2; ?></li>
+            <li><?php print $text_3; ?></li>
+            <li><?php print $text_4; ?></li>
             <hr>
-            <p><?php print $text_2; ?></p>
+            <li><?php print $text_6?></li>
         </ul>
     </body>
 </html>
