837b823c294929c66b757d2515eaff2881215dd5
arnasss <arnasile@gmail.com>
2019-09-13 10:36:32 +0300

    Keliones skaicevimas

--- index.php
+++ index.php
@@ -3,8 +3,18 @@
 $distance = rand(100, 1000);
 $consumption = 7.5;
 $price_l = 1.3;
-$sunaudota_kelioniai = ($distance * $consumption)/100;
-$keliones_kaina = $sunaudota_kelioniai * $price_l;
+$me_money = 100;
+
+$sunaudota_kelioniai = round(($distance * $consumption) / 100, 2);
+$keliones_kaina = round($sunaudota_kelioniai * $price_l, 2);
+if ($keliones_kaina < $me_money) {
+    $text_3 = 'įperkama';
+} else {
+    $text_3 = 'neįperkama';
+};
+
+$text_1 = "Turimi pinigai $me_money";
+$text_2 = "Išvada: Kelionė $text_3";
 
 ?>
 <html>
@@ -12,11 +22,10 @@ $keliones_kaina = $sunaudota_kelioniai * $price_l;
         <meta charset="UTF-8">
     </head>
     <body>
-        <h1>Kelionės skaičiouoklė</h1>
         <ul>
-            <li>Nuvažiuota distancija: <?php print $distance; ?></li>
-            <li>Sunaudota <?php print $sunaudota_kelioniai; ?> l. kuro.</li>
-            <li>Kaina <?php print $keliones_kaina; ?> pinuigų.</li>
+            <li><?php print $text_1; ?></li>
+            <hr>
+            <p><?php print $text_2; ?></p>
         </ul>
     </body>
 </html>
