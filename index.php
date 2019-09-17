b1a9032572dad26b6789683114ff8df238cbc5e8
arnasss <arnasile@gmail.com>
2019-09-17 12:27:06 +0300

    Masinos nuvertejmas V3

--- index.php
+++ index.php
@@ -2,17 +2,16 @@
 
 $months = 24;
 $car_price_new = 30000;
-$car_price_new_stable = $car_price_new;
+$car_price_used = $car_price_new;
 $depreciation = 2;
 
 $text_h2 = "Naujos mašinos kaina :$car_price_new";
 for($x = 0; $x < $months; $x++) {
-    $car_price_new -= ($car_price_new / 100) * $depreciation;
+    $car_price_used -= ($car_price_used / 100) * $depreciation;
 }
-$car_price_new = round($car_price_new);
-$depr_perc_not_r = ($car_price_new * 100) / $car_price_new_stable;
-$depr_perc = round($depr_perc_not_r);
-$text_h3 = "Po $months mėn, mašinos vertė bus: $car_price_new eur.";
+$car_price_used = round($car_price_used);
+$depr_perc= round(($car_price_used * 100) / $car_price_new);
+$text_h3 = "Po $months mėn, mašinos vertė bus: $car_price_used eur.";
 $text_h4 = "Mašina nuvertės $depr_perc proc.";
 
 ?>
