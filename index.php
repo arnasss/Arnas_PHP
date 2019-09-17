7329f96c3e7257d00a24659ac597bd712a46ad76
arnasss <arnasile@gmail.com>
2019-09-17 10:59:50 +0300

    Kiek isgivensi ant grikiu (teisingas)

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 
 $grikiai = 5000;
-$per_day = rand(200, 500);
-$grikiai_start = $grikiai;
-for($days = 1; $grikiai >= $per_day; $days++) {
-    $grikiai -= $per_day;
-}
-$data = date('Y/m/d', strtotime('+' .("$days days")));
-$text_h2 = "Rasta grikių: $grikiai_start g.";
+$days = 0;
+
+for($i = rand(200, 500); $i < $grikiai; $i += rand(200, 500), $days++);
+
+$data = date('Y/m/d', strtotime("+$days days"));
+$text_h2 = "Rasta grikių: $grikiai g.";
 $text_h3 = "Išgyvensiu dar $days dienas, iki $data";
 
 ?>
