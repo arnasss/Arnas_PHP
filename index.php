e86aae07033b06b29f99d2ae7cd5cb4d49c880db
arnasss <arnasile@gmail.com>
2019-09-17 12:08:45 +0300

    Masinos nuvertejmas

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <?php
 
-$grikiai = 5000;
-$days = 0;
+$months = 24;
+$car_price_new = 30000;
+$depreciation = 2;
 
-for($i = rand(200, 500); $i < $grikiai; $i += rand(200, 500), $days++);
-
-$data = date('Y/m/d', strtotime("+$days days"));
-$text_h2 = "Rasta grikių: $grikiai g.";
-$text_h3 = "Išgyvensiu dar $days dienas, iki $data";
+$text_h2 = "Naujos mašinos kaina :$car_price_new";
+for($x = 0; $x < $months; $x++) {
+    $car_price_new -= ($car_price_new / 100) * 2;
+}
+$car_price_new = round($car_price_new);
+$depr_perc_not_r = ($car_price_new * 100) / 30000;
+$depr_perc = round($depr_perc_not_r);
+$text_h3 = "Po $months mėn, mašinos vertė bus: $car_price_new eur.";
+$text_h4 = "Mašina nuvertės $depr_perc proc.";
 
 ?>
 <html>
     <body>
-        <h1>Kiek dienų galėsi valgyti grikius?</h1>
+        <h1>Kieks nuvertės mašina?</h1>
         <h2><?php print $text_h2; ?></h2>
         <h3><?php print $text_h3; ?></h3>
+        <h4><?php print $text_h4; ?></h4>
     </body>
 </html>
