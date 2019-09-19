e2964d91b93fdd76a7cd643f808829498ac9a545
arnasss <arnasile@gmail.com>
2019-09-19 12:37:06 +0300

    Banko israsai

--- index.php
+++ index.php
@@ -1,27 +1,33 @@
 <?php
 
-$car_price_new = 30000;
-$depreciation = 2;
-$santaoupos = 15000;
-$car_price_used = $car_price_new;
+$banko_report = [
+    [
+        'name' => 'IKI Darbo Užmokestis',
+        'amount' => 600
+    ],
+    [
+        'name' => 'Kalvarijų Načnykas',
+        'amount' => -15
+    ],
+    [
+        'name' => 'Circle K',
+        'amount' => -30
+    ],
+    [
+        'name' => 'Sweddbank bankomatas Antakalnis',
+        'amount' => -100
+    ]
+];
 
-for($months = 0; $car_price_used >= $santaoupos; $months++) {
-    $car_price_used -= ($car_price_used / 100) * $depreciation;
-}
-
-$car_price_used = round($car_price_used);
-$depr_perc= round(($car_price_used * 100) / $car_price_new);
-
-$text_h2 = "Naujos mašinos kaina :$car_price_new";
-$text_h3 = "Mašiną galėsiu nusipirkti po $months mėn. kai jos vertė bus: $car_price_used";
-$text_h4 = "Mašina nuvertės $depr_perc proc.";
+foreach($banko_report as $israso_idx => $israsas){
+    if ($israsas['amount'] > 0) {
+        $israsas['css_class'] = 'income'; 
+        $banko_report[$israso_idx]['css_class'] = 'income';
+    } else {
+        $banko_report[$israso_idx]['css_class'] = 'expense';
+    }    
+    }
+    
+var_dump($banko_report);
 
 ?>
-<html>
-    <body>
-        <h1>Kieks nuvertės mašina?</h1>
-        <h2><?php print $text_h2; ?></h2>
-        <h3><?php print $text_h3; ?></h3>
-        <h4><?php print $text_h4; ?></h4>
-    </body>
-</html>
