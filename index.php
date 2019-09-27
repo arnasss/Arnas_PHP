e648a9899c5bc6f630f554402803448c16fac79e
arnasss <arnasile@gmail.com>
2019-09-27 16:33:40 +0300

    avis

--- index.php
+++ index.php
@@ -1,84 +1,18 @@
 <?php
-$size = rand(3, 5);
 
-function slot_run($size) {
-    $array = [];
+$sheep = ['bleee'];
 
-    for ($x = 0; $x < $size; $x++) {
-        for ($y = 0; $y < $size; $y++) {
-            $array[$x][$y] = rand(0, 1);
-        }
-    }
-
-    return $array;
+for ($x = 0; $x < 4; $x++) {
+    $sheep[0] = 'tekstas';
+    $sheep[] = &$sheep[$x];
+}
+foreach ($sheep as $key => $shep) {
+    unset($sheep[$key]);
+    $sheep[$key] = $shep;
 }
 
-$matrix = slot_run($size);
 
-$matrix_display = [];
-foreach ($matrix as $row_idx => $row) {
-    $darbinis = 0;
-    
-    $matrix_display[$row_idx] = [
-        'class' => '',
-        'columns' => []
-    ];
-
-    foreach ($row as $column) {
-        if ($column == 0) {
-            $matrix_display[$row_idx]['columns'][] = [
-                'class' => 'bg-blue',
-            ];
-        } else {
-            $matrix_display[$row_idx]['columns'][] = [
-                'class' => 'bg-yellow',
-            ];
-            $darbinis++;
-        }
-    }
-    if ($darbinis == $size) {
-        $matrix_display[$row_idx]['class'] = 
-         'winning';
-            
-    }
-    
-}
+$sheep[3] = 'velniop sistema';
+var_dump($sheep);
 
 ?>
-<html>
-    <head>
-        <title>Las vegas </title>
-        <style>
-            .row.winning {
-                border: 1px solid red;
-            }
-
-            .column {
-                display: inline-block;
-                width: 50px;
-                height: 50px;
-                border: 1px solid black;
-            }
-
-            .bg-blue {
-                background-color: blue;
-            }
-
-            .bg-yellow {
-                background-color: yellow;
-            }
-
-        </style>
-    </head> 
-    <body>
-        <div class="slot-container">
-<?php foreach ($matrix_display as $row): ?>
-                <div class="row <?php print $row['class']; ?>">
-                <?php foreach ($row['columns'] as $column): ?>
-                        <div class="column <?php print $column['class']; ?>"></div>
-                    <?php endforeach; ?>
-                </div>
-                <?php endforeach; ?>
-        </div>
-    </body>
-</html>
