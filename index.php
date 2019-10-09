6abd2f13f67a4e97af18a2a65b748646ad52b25c
arnasss <arnasile@gmail.com>
2019-10-09 09:43:56 +0300

    templetas

--- index.php
+++ index.php
@@ -1,18 +1,211 @@
 <?php
 
-$sheep = ['bleee'];
+$form = [
+    'attr' => [
+        'action' => 'index.php',
+        'class' => 'bg-black'
+    ],
+    'title' => 'Kalėdų norai',
+    'fields' => [
+        'first_name' => [
+            'type' => 'text',
+            'label' => 'Vardas:',
+            'extra' => [
+                'attr' => [
+                    'placeholder' => 'Enter Name',
+                    'class' => 'input-text',
+                    'id' => 'first-name'
+                ]
+            ],
+            'validators' => [
+                'validate_not_empty'
+            ]
+        ],
+        'last_name' => [
+            'type' => 'text',
+            'label' => 'Pavardė:',
+            'extra' => [
+                'attr' => [
+                    'placeholder' => 'Enter Surname',
+                    'class' => 'input-text',
+                    'id' => 'last-name'
+                ]
+            ],
+            'validators' => [
+                'validate_not_empty'
+            ]
+        ],
+        'age' => [
+            'type' => 'text',
+            'label' => 'Metai:',
+            'extra' => [
+                'attr' => [
+                    'placeholder' => 'Enter Age',
+                    'class' => 'input-text',
+                    'id' => 'last-name'
+                ]
+            ],
+            'validators' => [
+                'validate_not_empty',
+                'validate_is_number',
+                'validate_is_positive',
+                'validate_max_100'
+            ]
+        ],
+        'wish' => [
+            'type' => 'select',
+            'value' => 'tv',
+            'label' => 'Kalėdom noriu:',
+            'extra' => [
+                'attr' => [
+                    'class' => 'input-select',
+                    'id' => 'wish'
+                ]
+            ],
+            'options' => [
+                'car' => 'BMW',
+                'tv' => 'Teliko',
+                'socks' => 'Kojinių'
+            ],
+            'validators' => [
+                'validate_not_empty'
+            ]
+        ]
+    ],
+    'buttons' => [
+        'submit' => [
+            'type' => 'submit',
+            'value' => 'Siųsti'
+        ],
+        'reset' => [
+            'type' => 'reset',
+            'value' => 'Išvalyti'
+        ]
+    ],
+    'message' => 'Užpildyk formą!',
+    'callbacks' => [
+        'fail' => 'form_fail',
+        'success' => 'form_success'
+    ]
+];
 
-for ($x = 0; $x < 4; $x++) {
-    $sheep[0] = 'tekstas';
-    $sheep[] = &$sheep[$x];
+/**
+ * Generates HTML attributes
+ * @param array $attr
+ * @return string
+ */
+function html_attr($attr) {
+    $html_attr_array = [];
+
+    foreach ($attr as $attribute_key => $attribute_value) {
+        $html_attr_array[] = strtr('@key="@value"', [
+            '@key' => $attribute_key,
+            '@value' => $attribute_value
+        ]);
+    }
+
+    return implode(' ', $html_attr_array);
 }
-foreach ($sheep as $key => $shep) {
-    unset($sheep[$key]);
-    $sheep[$key] = $shep;
+
+function validate_not_empty($field_input, &$field) {
+    if ($field_input === '') {
+        $field['error'] = 'Laukas negali būti tuščias!';
+        return false;
+    }
+
+    return true;
 }
 
+function validate_is_number($field_input, &$field) {
+    if (!is_numeric($field_input)) {
+        $field['error'] = 'Įveskite skaičių!';
+        return false;
+    }
 
-$sheep[3] = 'velniop sistema';
-var_dump($sheep);
+    return true;
+}
+
+function validate_max_100($field_input, &$field) {
+    if ($field_input > 100) {
+        $field['error'] = 'Per daug metų!';
+        return false;
+    }
+
+    return true;
+}
+
+function validate_is_positive($field_input, &$field) {
+    if ($field_input < 0) {
+        $field['error'] = 'Įveskite teigiamą skaičių!';
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * Sanitizes all form inputs
+ * @param array $form
+ * @return array
+ */
+function get_form_input($form) {
+    $filter_parameters = [];
+    foreach ($form['fields'] as $field_id => $field) {
+        $filter_parameters[$field_id] = $field['filter'] ?? FILTER_SANITIZE_SPECIAL_CHARS;
+    }
+
+    return filter_input_array(INPUT_POST, $filter_parameters);
+}
+
+function validate_form($filtered_input, &$form) {
+    $success = true;
+
+    foreach ($form['fields'] as $field_id => &$field) {
+        $field_input = $filtered_input[$field_id];
+        $field['value'] = $field_input;
+        foreach ($field['validators'] ?? [] as $validator) {
+            $is_valid = $validator($field_input, $field);
+            if (!$is_valid) {
+                $success = false;
+                break;
+            }
+        }
+    }
+
+    if ($success) {
+        if (isset($form['callbacks']['success'])) {
+            $form['callbacks']['success']($filtered_input, $form);
+        }
+    } else {
+        if (isset($form['callbacks']['fail'])) {
+            $form['callbacks']['fail']($filtered_input, $form);
+        }
+    }
+
+    return $success;
+}
+
+function form_fail($filtered_input, &$form) {
+    $form['message'] = 'Yra klaidų!';
+}
+
+function form_success($filtered_input, &$form) {
+    $form['message'] = 'success!';
+}
+
+$filtered_input = get_form_input($form);
+if (!empty($filtered_input)) {
+    validate_form($filtered_input, $form);
+}
 
 ?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Form Templates</title>
+        <link rel="stylesheet" href="includes/style.css">
+    </head>
+    <body>
+        <?php require 'templates/form.tpl.php'; ?>
+    </body>
+</html>
