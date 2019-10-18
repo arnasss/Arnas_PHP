b6ffd11189899ed10d1500515dde7256b7cb3956
arnasss <arnasile@gmail.com>
2019-10-18 12:51:33 +0300

    test

--- registration.php
+++ registration.php
@@ -55,6 +55,12 @@ $form = [
             ]
         ],
     ],
+//    'validators' => [
+//        'validate_fields_match' => [
+//            'password',
+//            'password_repeat'
+//        ]
+//    ],
     'buttons' => [
         'Registruotis' => [
             'type' => 'submit',
@@ -68,12 +74,24 @@ $form = [
     ]
 ];
 
-function form_success($filtered_input) {
-    $array = file_to_array('data/users.txt');
-    $array['info'] = $filtered_input;
-    array_to_file($array, 'data/users.txt');
+$filtered_input = get_form_input($form);
+
+if ($filtered_input) {
+    validate_form($filtered_input, $form);
 }
 
+function form_success($filtered_input) {
+    $forma = file_to_array('data/users.txt'); 
+    $forma[] = $filtered_input;
+    array_to_file($forma, 'data/users.txt');
+}
+
+$a = file_to_array('data/users.txt');
+var_dump($a);
+
+function validate_fields_match($filtered_input, $form, $params) {
+    if ()
+}
 
 ?>
 <html>
